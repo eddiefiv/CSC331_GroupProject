af0ac5f276849e26aa9a2d02e102ba27b5f9167a
eddiefiv <eddiefiv21@gmail.com>
2025-11-17 18:44:42 -0500

    Finish up the HandEvaulationResult class

--- Project/src/HandEvaluationResult.java
+++ Project/src/HandEvaluationResult.java
@@ -1,6 +1,25 @@
+/**
+ * Record to keep track of the evaulated HandType and the Cards associated with that HandType
+ *
+ * @author Eddie Falco
+ */
+
 import java.util.ArrayList;
 
 public class HandEvaluationResult {
-    private HandType handType;
-    private ArrayList<Card> cards;
+    private final HandType handType;
+    private final ArrayList<Card> cards;
+
+    public HandEvaluationResult(HandType handType, ArrayList<Card> cards) {
+        this.handType = handType;
+        this.cards = cards;
+    }
+
+    public HandType getHandType() {
+        return handType;
+    }
+
+    public ArrayList<Card> getCards() {
+        return cards;
+    }
 }
