1e6b1c9c4bc5aefb8de55b32069ab6838e88e81f
eddiefiv <eddiefiv21@gmail.com>
2025-11-18 00:08:50 -0500

    Driver testing

--- Project/src/Driver.java
+++ Project/src/Driver.java
@@ -2,14 +2,14 @@ public class Driver {
     public static void main(String[] args) {
         Player p1 = new Player(750);
 
-        p1.addCardToHand(new Card(CardSuit.HEART, CardRank.NINE));
-        p1.addCardToHand(new Card(CardSuit.SPADE, CardRank.TEN));
+        p1.addCardToHand(new Card(CardSuit.HEART, CardRank.TEN));
+        p1.addCardToHand(new Card(CardSuit.HEART, CardRank.EIGHT));
 
-        Table.board.add(new Card(CardSuit.SPADE, CardRank.NINE));
-        Table.board.add(new Card(CardSuit.DIAMOND, CardRank.FOUR));
-        Table.board.add(new Card(CardSuit.DIAMOND, CardRank.NINE));
-        Table.board.add(new Card(CardSuit.CLUB, CardRank.SIX));
-        Table.board.add(new Card(CardSuit.HEART, CardRank.KING));
+        Table.board.add(new Card(CardSuit.HEART, CardRank.NINE));
+        Table.board.add(new Card(CardSuit.SPADE, CardRank.QUEEN));
+        Table.board.add(new Card(CardSuit.SPADE, CardRank.JACK));
+        Table.board.add(new Card(CardSuit.HEART, CardRank.SIX));
+        Table.board.add(new Card(CardSuit.HEART, CardRank.SEVEN));
 
         HandEvaluationResult hand = HandEvaluator.evaluateHand(p1.getHand(), Table.board);
 
