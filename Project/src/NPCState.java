69921b0262b54e192bd48851dc2d1cf2982889ca
J22186 <Jmf7024@uncw.edu>
2025-11-23 16:39:56 -0500

    Comments
    Addes Javadoc comments

--- Project/src/NPCState.java
+++ Project/src/NPCState.java
@@ -1,5 +1,10 @@
 import java.util.EnumMap;
-
+/**
+* @author James Frink, Eddie Falco
+* CSC 331 Section 001 - 002 - 003
+* Date: 11/23/2025
+* Purpose: Establish the current state of an NPC
+*/
 public class NPCState {
     public double handStrength; // 0.0-1.0 value. How good is NPC's hand
     public int currentBet;
