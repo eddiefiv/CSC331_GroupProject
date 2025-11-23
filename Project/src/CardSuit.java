e2d726919565ecea73cd8ef787155ff6054fa663
J22186 <Jmf7024@uncw.edu>
2025-11-23 15:49:30 -0500

    Update CardSuit.java

--- Project/src/CardSuit.java
+++ Project/src/CardSuit.java
@@ -1,4 +1,11 @@
+/**
+* @author Emma Fox, Eddie Falco
+* CSC 331 Section 001 - 002 - 003
+* Date: 11/23/2025
+* Purpose: establish the suits of the cards
+*/
 public enum CardSuit {
+    //List of all suits
     SPADE,
     HEART,
     DIAMOND,
