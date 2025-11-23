b21fa1afc1236bd5830bf775e30be76a11d48907
J22186 <Jmf7024@uncw.edu>
2025-11-23 16:26:33 -0500

    Update ControllablePlayer.java
    Added Javadoc comments

--- Project/src/ControllablePlayer.java
+++ Project/src/ControllablePlayer.java
@@ -1,8 +1,22 @@
+/**
+* @author James Frink, Eddie Falco
+* CSC 331 Section 001 - 002 - 003
+* Date: 11/23/2025
+* Purpose: Create a builder for a controllable Player object
+*/
 public class ControllablePlayer extends Player {
+    /**
+    * ControllablePlayer Constructor
+    * @param playerName player's name (String)
+    */
     public ControllablePlayer(String playerName) {
         super(playerName);
     }
-
+    /**
+    * ControllablePlayer Constructor
+    * @param playerName player's name (String)
+    * @param balance starting money for the player (int)
+    */
     public ControllablePlayer(String playerName, int balance) {
         super(playerName, balance);
     }
